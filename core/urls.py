813d01e83492b1c9c12828640008099eb4baa81f
nchoriyev <nchoriyev43@gmail.com>
2025-03-10 02:50:20 +0500

    changes

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('home.urls')),
+    path('home/', include('home.urls')),
     path('', include('users.urls')),
 ]
 
